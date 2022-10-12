5d665ef4a2e8bfe4a560dbe75e387c4a66ac4e7a
iitianmohit3005 <phooldass201@gmail.com>
2022-10-13 00:56:24 +0530

    commit

---
+++ db.js
@@ -0,0 +1,95 @@
+const { Client } = require('pg')
+
+const client = new Client({
+    host: 'localhost',
+    user: 'guest',
+    password: 'guest',
+    database: 'bank_db',
+    post: 3000
+})
+
+client.connect(err => {
+    if (err) {
+        console.log(`❌ Error In Connectivity`)
+        return
+    }
+    console.log(`\n ✅ Connected Successfully`)
+})
+
+const createNewAccount = ({ acId, acNm, balance }, onCreate = undefined) => {
+    client.query(`insert into account values ($1, $2, $3)`, [acId, acNm, balance], (err, res) => {
+        if (err) console.log(`\n ❌ Problem In Creating the Customer`)
+        else {
+            console.log(`\n ✅ New Customer Created Successfully`)
+            if(onCreate) onCreate(`✅ New Customer Created Successfully`)
+        }
+    })
+}
+
+const withdraw = ({ acId, amount }, onWithdraw = undefined) => {
+    client.query(`select balance from account where ac_id = $1`, [acId], (err, res) => {
+        if (err) {
+            console.log(`\n ❌ Problem In Withdrawing`)
+        } else {
+            const balance = parseFloat(res.rows[0].balance)
+
+            const newBalance = balance - parseFloat(amount)
+
+            client.query(`update account set balance = $1 where ac_id = $2`, [newBalance, acId], (err, res) => {
+                if (err) console.log(`\n ❌ Problem In Withdrawing`)
+                else {
+                    console.log(`\n ✅ Amount ${amount} Withdrawal Successfully`)
+                    if(onWithdraw) onWithdraw(`✅ Amount ${amount} Withdraw Successfully`)
+                }
+            })
+        }
+    })
+}
+
+const deposit = ({ acId, amount }, onDeposit = undefined) => {
+    client.query(`select balance from account where ac_id = $1`, [acId], (err, res) => {
+        if (err) {
+            console.log(`\n ❌ Problem In Deposit`)
+        }
+        else {
+            const balance = parseFloat(res.rows[0].balance)
+            const newBalance = balance + parseFloat(amount)
+
+            client.query(`update account set balance = $1 where ac_id = $2`, [newBalance, acId], (err, res) => {
+                if (err) console.log(`\n ❌ Problem In Depositing`)
+                else  {
+                    console.log(`\n ✅ Amount ${amount} Deposited Successfully`)
+
+                    if(onDeposit) onDeposit(`✅ Amount ${amount} Deposited Successfully`)
+                }
+            })
+        }
+    })
+}
+
+const transfer = ( {srcId, destId, amount }, onTransfer = undefined) => {
+    withdraw({ acId : srcId, amount }, msgWd => {
+        deposit({ acId : destId, amount }, msgDp => {
+            if(onTransfer) onTransfer( `✅ Amount ${amount} Transferred Successfully` )
+        })
+    })
+}
+
+const balance  = (acId, onBalance = undefined) => {
+    console.log(acId)
+    client.query(`select balance from account where ac_id = $1`, [acId], (err, res) => {
+        if (err) {
+            console.log(`\n ❌ Problem In Fetching the balance`)
+            console.log(err)
+        } else {
+            const balance = parseFloat(res.rows[0].balance)
+            console.log(`\n 💰 Your Account Balance Is : ${balance}`)
+            if(onBalance) onBalance(balance)
+        }
+    })
+}
+
+module.exports = {
+    createNewAccount, deposit, withdraw, transfer, balance
+}
+
---
+++ web.js
@@ -0,0 +1,46 @@
+const { response } = require('express')
+const express = require('express')
+
+const cors = require('cors')
+const app = express()
+app.use(cors())
+
+const port = 3100
+
+const { createNewAccount, deposit, withdraw, balance, transfer } = require('./db')
+
+app.post('/create', express.json(), (req, res) => {
+    createNewAccount( req.body , (msg) => {
+        res.json({ 'sts' : 'success', msg })
+    })
+})
+
+app.put('/transfer', express.json() ,(req, res) => {
+    transfer(req.body, msg => {
+        res.json({ 'sts' : 'success', msg })
+    })
+})
+
+app.put('/withdraw', express.json(), (req, res) => {
+    withdraw(req.body, msg => {
+        res.json({ 'sts' : 'success', msg })
+    })
+})
+
+app.put( '/deposit', express.json() ,(req, res) => {
+    deposit(req.body, msg => {
+        res.json({ 'sts' : 'success', msg })
+    })
+} )
+
+app.get('/balance/:acId', ( req, res ) => {
+    console.log(req.params)
+    const acId = req.params.acId
+    balance(acId, bal => {
+        res.json({ bal })
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Banking App app listening on port ${port}`)
+})
